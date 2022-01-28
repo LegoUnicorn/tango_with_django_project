e2f266b335226ccbbb3707ff1eee0272979c6d4e
Alex Boestad <256887b@student.gla.ac.uk>
2022-01-28 15:06:37 +0000

    Static and media and such

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,15 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {
+        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'
+        }
+    return render(request, 'rango/index.html', context=context_dict)
+    # return HttpResponse("<a href='/rango/about'>Rango</a> says hey there partner!")
+
+def about(request):
+    context_dict = {
+        'boldmessage': 'This tutorial has been put together by Alex'
+        }
+    return render(request, 'rango/about.html', context=context_dict)
+    # return HttpResponse("<a href='/rango/'>Rango</a> says here is the about page.")
