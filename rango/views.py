7173a9d4e72daf02db8fea269696ea8878c698ea
Alex Boestad <256887b@student.gla.ac.uk>
2022-02-11 15:39:54 +0000

    added more cookies

--- rango/views.py
+++ rango/views.py
@@ -13,6 +13,7 @@ from django.http import HttpResponse
 from django.urls import reverse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 
@@ -25,15 +26,11 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+
+    response = render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request, response)
+
     return render(request, 'rango/index.html', context=context_dict)
- 
-    '''
-    context_dict = {
-        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'
-        }
-    return render(request, 'rango/index.html', context=context_dict)
-    # return HttpResponse("<a href='/rango/about'>Rango</a> says hey there partner!")
-    '''
 
 def about(request):
     if request.session.test_cookie_worked():
@@ -161,3 +158,15 @@ def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
     
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    response.set_cookie('visits', visits)
